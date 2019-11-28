000299eae245257952c856086534ada561fd605b
talat <change@gmail.com>
2019-11-28 13:02:33 +0530

    first commit

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,14 +2,12 @@ const id = "YOUR_CLIENT_ID"
 const sec = "YOUR_SECRET_ID"
 const params = `?client_id=${id}&client_secret=${sec}`
 
-
-function getErrorMessage(message, username) {
+function getErrorMsg(message, username) {
   if (message === 'Not Found') {
-    return `${username} doesnt exist`
+    return `${username} doesn't exist`
   }
   
   return message
-  
 }
 
 function getProfile(username) {
@@ -17,56 +15,52 @@ function getProfile(username) {
     .then((res) => res.json())
     .then((profile) => {
       if (profile.message) {
-        throw new Error(getErrorMessage(profile.message, username))
+        throw new Error(getErrorMsg(profile.message, username))
       }
+      
       return profile
     })
-  
 }
 
 function getRepos(username) {
   return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
-    .then((res) => res.json)
+    .then((res) => res.json())
     .then((repos) => {
       if (repos.message) {
-        throw new Error(getErrorMessage(profile.message, username))
+        throw new Error(getErrorMsg(repos.message, username))
       }
+      
       return repos
     })
-  
 }
 
-function getStartCount(repos) {
+function getStarCount(repos) {
   return repos.reduce((count, {stargazers_count}) => count + stargazers_count, 0)
-
 }
 
 function calculateScore(followers, repos) {
-  return (followers * 3) + getStartCount(repos)
-}
-
-function sortPlayers(players) {
-  return players.sort((a, b) => b.score - a.score)
-  
+  return (followers * 3) + getStarCount(repos)
 }
 
 function getUserData(player) {
-  return promise.all([
+  return Promise.all([
     getProfile(player),
     getRepos(player)
   ]).then(([profile, repos]) => ({
     profile,
     score: calculateScore(profile.followers, repos)
   }))
-  
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score)
 }
 
 export function battle(players) {
-  return promise.all([
+  return Promise.all([
     getUserData(players[0]),
     getUserData(players[1])
   ]).then((results) => sortPlayers(results))
-  
 }
 
 export function fetchPopularRepos(language) {
